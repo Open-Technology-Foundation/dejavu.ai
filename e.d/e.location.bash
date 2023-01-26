54243b51beedfc23a8d51d8bca406e1a4f80201c
GaryDean <garydean@okusi.id>
2023-01-26 17:37:45 +0800

    updates

--- e.d/e.location.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#@ Function: e.location
-#@ Desc    : Return with current setting for ENTITIES and PATH.
-#shellcheck disable=SC2154
-e.location() { 
-	echo "${ENTITIES@A};${PATH@A}"
-}
-declare -fx e.location
-#fin
--- e.d/sys/calcfp.bash
+++
@@ -1,8 +0,0 @@
-#X Function: calcfp 
-#X Desc    : Simple [bc] wrapper. Requires /usr/bin/bc
-#X Synopsis: calcfp numericExpression
-calcfp() { echo "$*" | /usr/bin/bc -l; }
-declare -fx calcfp
-
-#fin
-
