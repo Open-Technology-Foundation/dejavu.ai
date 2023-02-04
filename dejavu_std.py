25660a6b00b725ba6ce02801f13086048704e2b0
GaryDean <garydean@okusi.id>
2023-02-04 14:16:47 +0800

    update 0.9.41

--- dejavu_std.py
+++ dejavu_std.py
@@ -1,11 +1,17 @@
 import os
 import io
 import sys
+import readline
+import atexit
+import glob
 from colorama import init, Fore, Back, Style
 init()
 
-global useColor
 useColor = True
+def printUseColor(color=True):
+  global useColor
+  useColor = color
+  return useColor
 
 def printerr(*args, **kwargs):
   global useColor
@@ -35,16 +41,16 @@ def printstd(*args, **kwargs):
   if useColor: print(Style.RESET_ALL, end='', file=output)
 
 
-def file_get_contents(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
+def readfile(filepath, enc='utf-8'):
+    with open(filepath, 'r', encoding=enc) as infile:
         return infile.read()
 
-def file_put_contents(filename, string, mode='w'):
-    with open(filename, mode) as f:
+def writefile(filename, string, mode='w', enc='utf-8'):
+    with open(filename, mode, encoding=enc) as f:
         f.write(string)
 
 
-def is_numeric(string):
+def is_num(string):
   try:
     float(string)
     return True
@@ -54,10 +60,10 @@ def is_numeric(string):
 def int_list(input_string, minVal, maxVal, revSort=False):
   range_list = []
   try:
-    numlist = input_string.split(',')
+    numlist = input_string.lower().split(',')
     for numC in numlist:
       if numC.strip() == '': continue
-      if numC.strip() == 'all': numC = '1-'
+      if numC.strip() == 'all': numC = str(minVal)+'-'+str(maxVal)
       if '-' in numC:
         stnum, endnum = numC.split('-')
         if stnum == '':   stnum = int(minVal)
@@ -80,9 +86,8 @@ def int_list(input_string, minVal, maxVal, revSort=False):
   else:       return range_list
 
 
-global EDITOR, BROWSER
-EDITOR = ''; BROWSER = ''
-def getEDITOR():
+EDITOR = ''
+def getEditor():
   global EDITOR
   EDITOR = os.environ.get('EDITOR')
   if not EDITOR:
@@ -100,10 +105,13 @@ def getEDITOR():
         EDITOR = SELECTED_EDITOR
     elif os.path.isfile('/etc/alternatives/editor'):
       EDITOR = '/etc/alternatives/editor'
+    else:
+      EDITOR = '/bin/less'
   return EDITOR
-getEDITOR()
+getEditor()
 
-def getBROWSER():
+BROWSER = ''
+def getBrowser():
   global BROWSER
   BROWSER = os.environ.get('BROWSER')
   if not BROWSER:
@@ -114,5 +122,41 @@ def getBROWSER():
       BROWSER = '/usr/bin/w3m'
     elif os.path.isfile('/usr/bin/lynx'):
       BROWSER = '/usr/bin/lynx'
+    else:
+      BROWSER = '/bin/less'
   return BROWSER
-getBROWSER()
+getBrowser()
+
+
+# readline() history file +atExit
+def initHistory(filename, ext='.history'):
+  historyFile = os.path.dirname(filename)+'/.'+os.path.basename(filename)+ext
+  try:
+      readline.read_history_file(historyFile)
+      readline.set_history_length(200)
+  except FileNotFoundError:
+      writefile(historyFile, '')
+  atexit.register(readline.write_history_file, historyFile)
+  return historyFile
+
+
+def selectFile(dirs, prompt='Select File: ', home=''):
+  files = [ '' ]
+  for root in dirs:
+    for file in glob.glob("**/*.dv", root_dir=root, recursive=True):
+      files.append((root+'/'+file).replace(home, '~').replace('./',''))
+  selection = 0
+  for index, file in enumerate(files):
+    if index == 0: continue
+    printstd(str(index) + ': ' + file)
+  while True:
+    try:
+      selection = int(input(prompt))
+    except KeyboardInterrupt:
+      selection = 0; break
+    except:
+      printerr('Select 0-'+str(len(files)-1)); continue
+    if selection == 0: break
+    if selection >= 1 and selection < len(files): break
+    else: printerr('Invalid selection.')
+  return(files[selection])
--- tt.py
+++ tt.py
@@ -1,21 +1,63 @@
 import os
 import sys
-from colorama import init, Fore, Back, Style
-init()
-from markdown import markdownFromFile
-import random
 
-Back=[
-  Back.BLACK,
-  Back.RED,
-  Back.GREEN,
-  Back.YELLOW,
-  Back.BLUE,
-  Back.MAGENTA,
-  Back.CYAN,
-  Back.WHITE ]
+from std import file_get_contents, file_put_contents, \
+                EDITOR, BROWSER, \
+                int_list, is_numeric, \
+                useColor, printinfo, printerr, printstd, printUseColor
+#from colorama import init, Fore, Back, Style
+#init()
+#from markdown import markdownFromFile
+#import random
+cmdTypeAhead = []
+if True:
+  if True:
+    file = 'example-script'
 
-for b in Back:
-  print(Fore.WHITE+b+Style.NORMAL+'testing one two three'+Style.RESET_ALL)
-  print(Fore.WHITE+b+Style.DIM+'testing one two three'+Style.RESET_ALL)
+    #file = sys.argv.pop(0)
+    if not '.dv' in file: file += '.dv'
+    if not os.path.exists(file):
+      if not '/' in file: file = UserHome + '/' + file
+      if not os.path.exists(file):
+        printerr('File \''+file+'\' does not exist.')
+        exit()
 
+    lne = file_get_contents(file)
+    Lines = lne.split('\n')
+    lne   = ''
+    while len(Lines) > 0:
+      line = Lines.pop(0).rstrip()
+      if not line: continue
+      if line[0] == '#': continue
+      # handle \ line continuations
+      if line[-1] == '\\': lne += line[0:-1]; continue
+
+      lne += line.rstrip('\r\n')
+      if not lne: lne=''; continue
+      
+      if lne[0] == '!':
+        tok = lne.split()
+        if tok[0] == '!prompt' and len(tok) > 1:
+          if tok[1] != '"""': 
+            cmdTypeAhead.append('!prompt '+' '.join(tok[1:]))
+            lne = ''
+            continue
+          prompt = ''
+          while len(Lines) > 0:
+            line = Lines.pop(0).rstrip()
+            if line == '"""':
+              cmdTypeAhead.append('!prompt '+prompt)
+              break
+            prompt += line + '\\n'
+        else:
+          cmdTypeAhead.append(' '.join(tok))
+        lne = ''
+        continue
+      cmdTypeAhead.append(lne)
+      lne = ''
+    lne = lne.rstrip('\r\n')
+    if lne: cmdTypeAhead.append(lne)
+    lne = ''
+#printinfo('[start '+file+']')
+printinfo('\n'.join(cmdTypeAhead), prefix='')
+#printinfo('[end]')
